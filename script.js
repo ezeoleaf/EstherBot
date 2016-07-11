a6a2036a4d909522ff3ba71e3b3c83858644d82e
Eze BIT <efigueroa@bit.com.ar>
2016-07-11 12:15:57 -0300

    Add text

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ Is that OK? %[Yes](postback:yes) %[No](postback:no)`))
 
             function replaceTags(bot,text)
             {
-                return 'Probando reemplazo de tags' + botGen.getProp('namePerson') + bot;
+                return 'Probando reemplazo de tags' + botGen.getProp('namePerson') + getSilent();
             }
 
             function processMessage(isSilent) {
